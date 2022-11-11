0ea74eb7dfa5bcfd4b477d657899a8454c87d7c4
Gonza <106858958+M4CH1N3G1RL@users.noreply.github.com>
2022-11-10 22:59:45 -0600

    Delete src directory

--- src/TCP_IP_COMM/tests/ClientServerUDP/Client_UDP.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-bufferSize=1024
-HOST = 'localhost'               
-PORT = 50007                       # Arbitrary non-privileged port
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    s.sendall(b'holaa ')
--- src/TCP_IP_COMM/tests/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),2001))
-
-while True:
-    msg = s.recv(62)
-    print(msg.decode("utf-8"))
